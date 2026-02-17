355632cc2241a4a7a589f03f91f635bab41df21a
Celeste <vrtx65535@protonmail.com>
2026-02-16 20:53:11 -0700

    prompts working
    this took way too long. :(

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 # Using code from https://www.geeksforgeeks.org/python/flask-web-sockets/ as starter code
 
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO, send, emit, join_room, leave_room
+import flask
+from flask_socketio import SocketIO, send, emit, join_room, leave_room, close_room
 import os
 import random
 
@@ -20,7 +21,7 @@ prompts = []
 rooms = {} # store room objects by room id
 user_room = {} # store the room id the user is currently in, by username
 usernames = {} # store username by user id
-test = "def"
+
 
 class Room():
     def __init__(self, room_id):
@@ -29,81 +30,73 @@ class Room():
         self.room_id = room_id # first person's id determines room ID
         self.prompt = "[prompt]"
 
-    def newprompt(self):
+    def new_prompt(self):
         num_prompts = len(prompts)
 
-        prompt_number = random(0, num_prompts - 1)
+        prompt_number = random.randint(0, num_prompts - 1)
 
         self.prompt = prompts[prompt_number]
 
 
-@app.route('/queue')
-def queue():
-    return render_template('debate.html', prompttext=test)
-    
+@app.route('/debate')
+def debate():
+    return render_template('debate.html')
+
+
 @app.route('/')
 def index():
     image = "static/logo.png"
     return render_template('index.html', imageurl = image)
 
+
 # Handle new user joining
 @socketio.on('join')
 def handle_join(username):
-    global test
     usernames[request.sid] = username  # Store username by session ID
-    if DEBUG:
-        print(f"\n[DEBUG] Username: {username}, request.sid: {request.sid}\n")
+
     room_assigned = None
     for room in rooms.values():
-        if DEBUG:
-            print(f"[DEBUG] \n{room.room_id}'s room has {len(room.debaters)} people.\n")
         if len(room.debaters) == 1:
-
             room_assigned = room # add this user to list of debaters
             join_room(room_assigned.room_id)
             room.debaters.append(username)
             rooms[room_assigned.room_id] = room_assigned
             user_room[username] = room_assigned.room_id
-
-            if DEBUG:
-                print(f"[DEBUG] \n{username} joining existing room ({room_assigned.room_id}).\n")
-
             break
             
     if room_assigned is None: # if there is nobody waiting for a match
         room_assigned = Room(username) # create new room and make this user the first debater in it
         rooms[username] = room_assigned
+        room_assigned.new_prompt()
         join_room(username)
         user_room[username] = username
-        if DEBUG:
-                print(f"\n{username} joining new room ({username}).\n")
 
+    # Runs regardless
     send(f"{username} joined the chat", to=room_assigned.room_id)
+    emit("prompt_update", room_assigned.prompt) # trigger "prompt_update" event
 
+    
 # Handle user messages
 @socketio.on('message')
 def handle_message(data):
     username = usernames.get(request.sid, "Anonymous")  # Get the user's name
     room_id = user_room[username]
-    send(f"{username}: {data}", to=room_id)  # Send to everyone
+    send(f"{username}: {data}", to=room_id)  # Send to everyone, event is "message"
+    
 
 # Handle disconnects
 @socketio.on('disconnect')
 def handle_disconnect(data):
     username = usernames.pop(request.sid, "Anonymous")
-    
     room_id = user_room.get(username, None)
 
-    if room_id != None:
-        
+    if room_id is not None:      
         send(f"{username} left the chat", to=room_id)
-
-        if DEBUG:
-            print(f"{username} left the chat")
+        rooms[room_id].debaters.remove(username)
         
-            rooms[room_id].debaters.remove(username)
-            if len(rooms[room_id].debaters == 0):
-                del rooms[room_id]
+        if len(rooms[room_id].debaters) == 0:
+            close_room(room_id)
+            del rooms[room_id]
     
 
 if __name__ == '__main__':
